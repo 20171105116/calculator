5269f49b2b9ec9376e02947955621d80288f07c6
20171105116 <1343452891@qq.com>
2018-10-11 09:17:36 +0800

    caculater
     实现单一的加减乘除

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,7 +10,107 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x = 0
     var temp = 0
+    
+    @IBOutlet weak var dateframe: UITextField!
+    
+    @IBAction func button1(_ sender: Any) {
+        dateframe.text = dateframe.text! + "1"
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        dateframe.text = dateframe.text! + "2"
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        dateframe.text = dateframe.text! + "3"
+    }
+    
+    @IBAction func button4(_ sender: Any) {
+        dateframe.text = dateframe.text! + "4"
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+        dateframe.text = dateframe.text! + "5"
+    }
+    
+    @IBAction func button6(_ sender: Any) {
+        dateframe.text = dateframe.text! + "6"
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+        dateframe.text = dateframe.text! + "7"
+    }
+    
+    @IBAction func button8(_ sender: Any) {
+        dateframe.text = dateframe.text! + "8"
+    }
+    
+    @IBAction func button9(_ sender: Any) {
+        dateframe.text = dateframe.text! + "9"
+    }
+    
+    @IBAction func button0(_ sender: Any) {
+        dateframe.text = dateframe.text! + "0"
+    }
+    
+    
+    @IBAction func clear(_ sender: Any) {
+        temp = 0
+        dateframe.text = ""
+    }
+    @IBAction func add(_ sender: Any) {
+        x = 1
+        temp = temp + Int(dateframe.text!)!
+        dateframe.text = ""
+        
+    }
+    
+    @IBAction func caculater(_ sender: Any) {
+        if(x == 1)
+        {
+        var sum = 0
+        sum = temp + Int(dateframe.text!)!
+        dateframe.text = "\(sum)"
+        }
+        if(x == 2)
+        {
+            var sum = 0
+            sum = temp - Int(dateframe.text!)!
+            dateframe.text = "\(sum)"
+        }
+        if(x == 3)
+        {
+            var sum = 0
+            sum = temp * Int(dateframe.text!)!
+            dateframe.text = "\(sum)"
+        }
+        if(x == 4)
+        {
+            var sum = 0
+            sum = temp * Int(dateframe.text!)!
+            dateframe.text = "\(sum)"
+        }
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        x = 2
+        temp =  Int(dateframe.text!)!
+        dateframe.text = ""
+    }
+    
+    @IBAction func ride(_ sender: Any) {
+        x = 3
+        temp =  Int(dateframe.text!)!
+        dateframe.text = ""
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        x = 4
+        temp =  Int(dateframe.text!)!
+        dateframe.text = ""
+    }
     
     
     
